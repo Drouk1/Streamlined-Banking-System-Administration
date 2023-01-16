d6b36e066f4913768bb6f2d899ca5a512657154f
Drouk1 <andy.orfanidis@gmail.com>
2023-01-16 21:19:22 +0200

    making the menu and the new seller section in main

--- Main.java
+++ Main.java
@@ -5,5 +5,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println( // MENU
+                "Choose:\n\t1.new seller\n\t2.new bank product\n\t3.new sale\n\t4.new credit card movement\n\t5.show loans\n\t6.calculating a seller's commission\n\t7.Display of credit card transactions related to a seller\n\t8.Calculation of the commission of all the sellers of the bank\n\t9.Display the final commission amount of all sellers\n\n\tOr 0 to end the programm!");
+        int choice = scan.nextInt();
+
+        
+
+        while (choice < 0 || choice > 9) { //if answer is not between 0-9 it asks again till it gets the right answer
+            System.out.println("Please type a number between 1 - 9 or 0 to end the programm!");
+            choice = scan.nextInt();
+        }
+
+        while (choice != 0) {
+
+            if (choice == 1) { // input apo xristi
+                scan.nextLine();
+                System.out.print("First name: ");
+                String firstName = scan.nextLine();
+
+                System.out.print("Last name: ");
+                String lastName = scan.nextLine();
+
+                System.out.print("Code: ");
+                int code = scan.nextInt();
+
+                System.out.print("AFM: ");
+                int afm = scan.nextInt();
+
+                Sellers seller = new Sellers(firstName, lastName, afm, code); // makes new seller object with the users input
+                                                                              
+                //sellersStore.getSellers().add(seller);
+
+                System.out.println("SELLER ADDED");
+
+            }
+        }
     }    
 }
